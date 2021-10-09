aa3e5ef0b1b3f489a474bf3d60435344303f021b
Sanel K <20467576+sanelk2004@users.noreply.github.com>
2021-10-09 00:00:43 -0400

    Oops
    That's what I get for pushing straight to production...

--- src/Page.js
+++ src/Page.js
@@ -4,7 +4,7 @@ const Page = (props) => {
     const is_debug = process.env.NODE_ENV !== 'production';
     useEffect(() => {
         // eslint-disable-next-line
-        is_debug ? document.title = "[DEBUG MODE] " + (props.title || "") + " :: Sanel Kukic" : new_title = (props.title || "") + " :: Sanel Kukic";
+        is_debug ? document.title = "[DEBUG MODE] " + (props.title || "") + " :: Sanel Kukic" : document.title = (props.title || "") + " :: Sanel Kukic";
     }, [props.title, is_debug]);
     return props.children;
 }
